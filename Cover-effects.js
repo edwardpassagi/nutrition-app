93943d5b2d643e19754197f760951dd8e74b34e1
hhache2 <hhache2@illinois.edu>
2020-10-27 15:46:32 -0500

    initial front page for our website

---
+++ Cover-effects.js
@@ -0,0 +1,52 @@
+var first_click = false;
+var second_click = false; 
+var clicks = 0
+
+$(document).ready(function() {
+    
+    $(window).ready(function() {
+        document_initiation();
+        button_click_setting();
+    });
+
+    $(window).resize(function() {
+        // document_resized();
+    });
+
+    $(document).scroll(function() {
+        // documnet_scrolled();
+    });
+});
+
+function document_initiation() {
+    hide_elements();
+
+    setTimeout(function() {
+        $('.inner h1').fadeIn(1000);
+    }, 1100);
+
+    setTimeout(function() {
+        $('.inner h2').fadeIn(1000);
+    }, 2000);
+
+    setTimeout(function() {
+        $('.inner button').fadeIn(500);
+    }, 2000);
+}
+
+function hide_elements() {
+    $('.inner h1').hide();
+    $('.inner h2').hide();
+    $('.inner button').hide();
+}
+
+function button_click_setting() {
+    $('.inner button').click(function() {
+        if (clicks == 0) {
+            clicks = 1;
+            $('.inner button').css('color', 'white');
+        } else {
+            $('.inner a').attr('href', '/Main/main.html');
+        }
+    });
+}
