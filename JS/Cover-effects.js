543948c1781bee49e55590d61e3aa474e5fda5a5
hhache2 <hhache2@illinois.edu>
2020-10-27 20:54:09 -0500

    imporovemed spacing and fixed absolute positioning

--- JS/Cover-effects.js
+++ JS/Cover-effects.js
@@ -7,10 +7,21 @@ $(document).ready(function() {
     $(window).ready(function() {
         document_initiation();
         button_click_setting();
+        var num = $('.inner h3').width();
+        // console.log(num);
+        set_header3_position();
     });
 
     $(window).resize(function() {
         // document_resized();
+        console.log('\n');
+        set_header3_position();
+        var header1_font_size = $('.inner h1').css('font-size');
+        console.log("header 1 font size: " + header1_font_size);
+        var header2_font_size = $('.inner h2').css('font-size');
+        console.log("header 2 font size: " + header2_font_size);
+        var header3_font_size = $('.inner h3').css('font-size');
+        console.log("header 3 font size: " + header3_font_size);
     });
 
     $(document).scroll(function() {
@@ -54,4 +65,13 @@ function button_click_setting() {
             $('.inner a').attr('href', '/Main/main.html');
         }
     });
+}
+
+function set_header3_position() {
+    var window_width = $('body').width();
+    var header3_width = $('.inner h3').width();
+    
+    console.log("header 3 width: " + header3_width);
+    console.log("window width: " + window_width);
+    $('.inner h3').css('left', window_width/2 - header3_width/2);
 }
