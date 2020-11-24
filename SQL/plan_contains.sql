be48e470656903534e241659f48fba9d08845049
Edward Passagi <edward.passagi@gmail.com>
2020-11-23 22:29:30 -0600

    backend to load foods for each meal (#14)
    * backend to load foods for each meal
    
    * removed unused comments
    
    * fixed sql insertion ordering
    
    * add insertion to each table in a single file

--- SQL/plan_contains.sql
+++ SQL/plan_contains.sql
@@ -1,15 +1,5 @@
--- TABLE CREATION
-CREATE TABLE plan_contains
-(
-  plan_id INT NOT NULL,
-  meal_id INT NOT NULL
-);
-
-
--- INSERT VALUES
 INSERT INTO plan_contains(plan_id, meal_id)
 VALUES(1,1);
-
 INSERT INTO plan_contains(plan_id, meal_id)
 VALUES(1,2);
 INSERT INTO plan_contains(plan_id, meal_id)
