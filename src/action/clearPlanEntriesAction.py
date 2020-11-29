53e1812a7c8084e5caae89499584c8801bec3cc8
edwardpassagi <edward.passagi@gmail.com>
2020-11-28 20:15:31 -0600

    fixed calories arithmetic error, dangling meals on regenerate plan

---
+++ src/action/clearPlanEntriesAction.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.insert(1, './')
+
+import src.action.removeMealAction as removeMealAction
+
+import src.dao.processPlanDAO as processPlanDAO
+import src.action.processPlanContainsAction as processPlanContainsAction
+
+def deletePlanIdEntry(pid):
+    # set pid planCalories to 0
+    # processPlanDAO.setPlanCaloriesFromPlanIdDAO(pid, 0)
+    # delete mid entries from pid
+    mids = processPlanContainsAction.getMealIdsFromPid(pid)
+    for mid in mids:
+        print(pid,mid)
+        removeMealAction.removeMealByID(pid,mid)
+    # Delete pid from planContains
+    # processPlanContainsDAO.deletePlanIdEntryDAO(pid)
+    return
---
+++ src/action/removeMealAction.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(1, './')
+
+import src.action.processPlanAction as processPlanAction
+import src.action.processMealAction as processMealAction
+
+import src.dao.processMealDAO as processMealDAO
+import src.dao.processMealContainsDAO as processMealContainsDAO
+import src.dao.processPlanContainsDAO as processPlanContainsDAO
+
+
+
+def removeMealByID(pid, mid):
+    # TODO: subtract plan calories from the mealCalories
+    meal = processMealAction.getMealByMealId(mid)
+    mealCalories = meal[0]['meal_calories']
+    print("MEAL CALORIES: {}".format(mealCalories))
+    updateVal = "-" + str(mealCalories)
+
+    processPlanAction.updatePlanCaloriesByPlanId(pid, updateVal)
+    processMealDAO.removeMealByIdDAO(mid)
+    processMealContainsDAO.removeMealIdFromMealContains(mid)
+    processPlanContainsDAO.removeMealIdFromPlanContains(mid)
+    return
